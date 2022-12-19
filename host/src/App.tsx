0adb7dd791129c4c2d2d6bb05b962c15eb72765f
Ram kumar <4328468+ramthenmala@users.noreply.github.com>
2022-12-19 22:39:25 +0000

    asynchronous method
    asynchronous method

--- host/src/App.tsx
+++ host/src/App.tsx
@@ -1,16 +1,27 @@
-import React from "react";
+import React, { Suspense, useState } from "react";
 import ReactDOM from "react-dom";
 
 import "./index.scss";
 
-import AppFooter from "PageComponents/Footer";
-import AppHeader from "PageComponents/Header";
+const Header = React.lazy(() => import("PageComponents/Header"));
+// const AppFooter = React.lazy(() => import("PageComponents/Footer"));
 
-const App = () => (
-  <>
-    <AppHeader/>
+// import AppHeader from "PageComponents/Header";
+import AppFooter from "PageComponents/Footer";
+
+const App = () => {
+  const [showHeader, setShowHeader] = useState<boolean>(false)
+  return (
+    <>
+      {
+        showHeader && <Suspense fallback={<div>Loading</div>}>
+        <Header />
+      </Suspense>
+      }
+      <button onClick={() => setShowHeader(!showHeader)}>Show header</button>
       <div>Front End</div>
-    <AppFooter/>
-  </>
-);
+      <AppFooter />
+    </>
+  )
+}
 ReactDOM.render(<App />, document.getElementById("app"));
